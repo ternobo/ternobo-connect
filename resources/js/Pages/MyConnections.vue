e81760f34c81a8d0bedcd926d60258f543085899
unknown <ali.parvizi@hotmail.com>
2020-10-02 06:25:05 +0330

    work on MyConnections

--- resources/js/Pages/MyConnections.vue
+++ resources/js/Pages/MyConnections.vue
@@ -1,18 +1,56 @@
 <template>
 <base-layout>
-    <div class="content-container-full">
-        Here is Content
-    </div>
-    <sidebar-left class="d-none d-lg-block">
-        Here is Sidebar
-    </sidebar-left>
+
+        <div class="w-100">
+            <h6 class="w-25 font-18 text-center pb-1" style="width: 10%!important; border-bottom:3px solid black">دعوت‌ها</h6>
+
+            <div class="row">
+
+                <div class="col-6">
+
+                    <b-card>
+                        <div class="row">
+                            <div class="col-md-2 col-12"><img src="https://www.alchinlong.com/wp-content/uploads/2015/09/sample-profile.png" class="rounded-circle profile-img"></div>
+                            <div class="col-md-6 col-12">
+                                <h6>سپهر حدائق‌نیا</h6>
+                                <h6>مدیرعامل و موسس ترنوبو</h6>
+                                <p><span class="material-icons font-18">location_on</span>یران، بوشهر</p>
+                            </div>
+                            <div class="col-md-4 col-12"><ConnectionButton class="float-left w-100"></ConnectionButton></div>
+                        </div>
+                    </b-card>
+
+
+                </div>
+                <div class="col-6">
+
+                    <b-card>
+                        <div class="row">
+                            <div class="col-4">img</div>
+                            <div class="col-4">wewewe</div>
+                            <div class="col-4"><button class="btn">دنبال کنید </button></div>
+                        </div>
+                    </b-card>
+
+                </div>
+
+
+            </div>
+
+
+        </div>
+
 </base-layout>
 </template>
 
 <script>
 import AppLayout from "../Layouts/AppLayout";
+import ConnectionButton from "../Components/buttons/ConnectionButton"
 export default {
     name: "MyConnectons",
+    components: {
+        ConnectionButton
+    },
     props: {
         followers_count: {
             type: Number,
@@ -43,6 +81,12 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+    .profile-img{
+        width: 70px;
+        height: 70px;
+    }
+    .card-body{
+        padding: 0.8rem 1.3rem;
+    }
 </style>
