3f6d3f25d918cc886258f88d4111ac9baee80803
Soroosh Tanzadeh <soroosh@ternobo.com>
2021-04-23 14:09:08 +0430

    currencies added

---
+++ resources/lang/en/currencies.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    "AUD" => "Australian dollar	",
+    "BRL" => "Brazilian real",
+    "CAD" => "Canadian dollar	",
+    "CNY" => "Chinese Renmenbi",
+    "CZK" => "Czech koruna",
+    "DKK" => "Danish koruna",
+    "EUR" => "Euro",
+    "HKD" => "Hong Kong dollar",
+    "HUF" => "Hungarian forint",
+    "JPY" => "Japanese yen",
+    "IRR" => "Iranian Rial",
+    "MYR" => "Malaysian ringgit",
+    "MXN" => "Mexican peso",
+    "TWD" => "New Taiwan dollar",
+    "NZD" => "New Zealand dollar",
+    "NOK" => "Norwegian krone",
+];
