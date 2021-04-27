545aabfe85bd56662741d0f13bcfc9d2285416cf
Soroosh Tanzadeh <soroosh@ternobo.com>
2021-04-27 15:05:02 +0430

    bugs fixed

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Str;
 use Ternobo\TernoboWire\TernoboWire;
 use Throwable;
 
@@ -40,7 +41,7 @@ class Handler extends ExceptionHandler
 
     protected function unauthenticated($request, AuthenticationException $exception)
     {
-        if ($request->expectsJson()) {
+        if ($request->expectsJson() || Str::contains($request->url(), "/api")) {
             return response()->json(['error' => 'Unauthenticated.'], 401);
         }
         return route('welcome');
