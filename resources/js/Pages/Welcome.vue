55e897bba7077043d614d629fee7bda7998695aa
Soroosh Tanzadeh <soroosh@ternobo.com>
2021-04-29 11:16:09 +0430

    welcom page bug fixed

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="container-fluid">
+	<div class="container-fluid px-3">
 		<LoginModal :show.sync="showLogin"></LoginModal>
 		<LawsModal :show.sync="showLaws"></LawsModal>
 		<SignupModal :show.sync="showSignup"></SignupModal>
