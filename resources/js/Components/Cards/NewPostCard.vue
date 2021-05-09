39f738b05b73b1142409d8e36f370ee6f70c5372
Soroosh Tanzadeh <soroosh@ternobo.com>
2021-05-10 01:13:46 +0430

    bugs fixed

--- resources/js/Components/Cards/NewPostCard.vue
+++ resources/js/Components/Cards/NewPostCard.vue
@@ -3,7 +3,7 @@
 		<new-post-modal @done="$emit('done')" :show.sync="showPostModal"></new-post-modal>
 		<div class="new-post-body">
 			<div class="text d-flex align-items-center">
-				<lazy-image img-class="profile-sm" class="profile-sm ml-2 mb-0" loading="lazy" :src="$store.state.user.profile" />
+				<lazy-image img-class="profile-sm" class="profile-sm mb-0" loading="lazy" :src="$store.state.user.profile" />
 				<div class="w-100 clickable" style="border-radius: 12px; background: #f5f5f5; padding: 8px 16px" @click="showPostModal = true">
 					<span class="new-post--placeholder">
 						{{ "به چی فکر می‌کنی " + $store.state.user.first_name + "؟" }}
