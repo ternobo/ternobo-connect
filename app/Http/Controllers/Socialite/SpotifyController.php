fdc703b78412c4b27b336cae25f3ef2f604bb5ef
Soroosh Tanzadeh <soroosh@ternobo.com>
2021-05-15 15:56:49 +0430

    spotify controller updated

--- app/Http/Controllers/Socialite/SpotifyController.php
+++ app/Http/Controllers/Socialite/SpotifyController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Socialite;
 use App\Http\Controllers\Controller;
 use App\Models\ConnectedAccount;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
 use Laravel\Socialite\Facades\Socialite;
 
 class SpotifyController extends Controller
@@ -20,6 +21,11 @@ class SpotifyController extends Controller
     {
         ConnectedAccount::query()->where("user_id", Auth::user()->id)->where("driver", "spotify")->delete();
         $user = Socialite::driver('spotify')->user();
+        $token = $user->token;
+        $response = Http::withHeaders([
+            "Authorization" => "Bearer $token",
+        ])->post('https://api.spotify.com/v1/me');
+
         ConnectedAccount::create([
             'name' => $user->name,
             'driver' => 'spotify',
@@ -27,11 +33,12 @@ class SpotifyController extends Controller
             'user_id' => Auth::user()->id,
             'expiresIn' => "9000",
             'meta' => [
-                'value' => $user->id,
+                'value' => $response->href,
                 'email' => $user->email,
                 'id' => $user->id,
             ],
         ]);
+        dd($response);
         event(new ReloadSocialOptions(Auth::user()));
         return view("onOAuthDone");
     }
