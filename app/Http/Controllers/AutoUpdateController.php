8ed468958757aaa7a64bc6fb5a6728a8d051363d
Soroosh Tanzadeh <soroosh@ternobo.com>
2021-05-23 12:50:52 +0430

    AutoUpdateController updated

--- app/Http/Controllers/AutoUpdateController.php
+++ app/Http/Controllers/AutoUpdateController.php
@@ -10,8 +10,8 @@ class AutoUpdateController extends Controller
     {
         if ($request->secret == env("GIT_SECRET")) {
             $path = base_path();
-            $output = exec("cd $path && git pull && npm run production &" . ' > /dev/null 2>&1 & echo $!');
-            return response()->json(['job' => "pid $output", "command" => "cd $path && git pull && npm run production &" . ' > /dev/null 2>&1 & echo $!']);
+            $output = exec("cd $path && git pull && npm run production");
+            return response()->json(['job' => "pid $output", "command" => "cd $path && git pull && npm run production"]);
         }
         return response()->json(['msg' => 'wrong secret', "data" => $request->secret], 401);
     }
