541bd0dfce04023ed808ce0f17d279fbae3b97e8
SepehrHN <sepehrhadaeghniapv@gmail.com>
2021-06-06 20:09:08 +0430

    All string keys updated to new structure + EN fully translated

---
+++ resources/lang/en/auth.php
@@ -0,0 +1,32 @@
+<?php
+return [
+    'create-account' => "Create Account",
+    'login' => "Login",
+    'signup' => "Signup",
+    'signout' => "Logout",
+    'password' => "Password",
+    'username' => "Username",
+    'forgot-password' => "Forgot password?",
+    'recover-password' => "Reset Password",
+    'send-recoverCode' => "Send password reset code",
+    'male' => "Male",
+    'female' => "Female",
+    'gender' => "Gender",
+    'first-name' => "First Name",
+    'last-name' => "Last Name",
+    'further-information' => "Personal Information",
+    'next' => "Next",
+    'security-tips' => "Security Tips",
+    'enter-your-picture' => "Choose a picture",
+    
+    'failed' => 'These credentials do not match our records.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    
+    'reset' => 'Your password has been reset!',
+    'sent' => 'We have emailed your password reset link!',
+    'throttled' => 'Please wait before retrying.',
+    'token' => 'This password reset token is invalid.',
+    'user' => "We can't find a user with that email address.",
+
+
+];
