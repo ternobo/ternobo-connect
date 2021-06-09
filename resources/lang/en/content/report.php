937c5574e2718f3dd02367a68370d30c243cbe58
Soroosh Tanzadeh <soroosh@ternobo.com>
2021-06-10 00:41:57 +0430

    merge sepehr

--- resources/lang/en/content/report.php
+++ resources/lang/en/content/report.php
@@ -6,6 +6,8 @@ return [
 
     'report-modal-title' => "Why are you reporting this?",
 
+    'description-report' => "Description",
+
     'rep-reason-1' => [
         'text' => "Suspicious or fake",
         'subreasons' => [
@@ -21,46 +23,64 @@ return [
             'rep-des-15' => "Unauthorized account takeover",
         ],
     ],
-    'rep-reason-2' => "Harassment or hateful speech",
-    'rep-reason-3' => "Violence of physical harm",
-    'rep-reason-4' => "Adult content",
-    'rep-reason-5' => "Child abuse",
-    'rep-reason-6' => "Intellectual property infringement or defamation",
 
-    'rep-title-21' => "Bullying or trolling",
-    'rep-des-21' => "Attacking or intimidating others, or deliberately and repeatedly disrupting conversations",
-    'rep-title-22' => "Sexual harassment",
-    'rep-des-22' => "Unwanted romantic advances, requests for sexual favors, or unwelcome sexual remarks",
-    'rep-title-23' => "Hateful or abusive speech",
-    'rep-des-23' => "Hateful, degrading, or inflammatory speech",
-    'rep-title-24' => "Spam",
-    'rep-des-24' => "Sharing irrelevant or repeated content to boost visibility or for monetary gain",
+    'rep-reason-2' => [
+        'text' => "Harassment or hateful speech",
+        'subreasons' => [
+            'rep-title-21' => "Bullying or trolling",
+            'rep-des-21' => "Attacking or intimidating others, or deliberately and repeatedly disrupting conversations",
+            'rep-title-22' => "Sexual harassment",
+            'rep-des-22' => "Unwanted romantic advances, requests for sexual favors, or unwelcome sexual remarks",
+            'rep-title-23' => "Hateful or abusive speech",
+            'rep-des-23' => "Hateful, degrading, or inflammatory speech",
+            'rep-title-24' => "Spam",
+            'rep-des-24' => "Sharing irrelevant or repeated content to boost visibility or for monetary gain",
+        ],
+    ],
 
-    'rep-title-31' => "Incites violence or is a threat",
-    'rep-des-31' => "Encouraging violent acts or threatening physical harm",
-    'rep-title-32' => "Self-harm",
-    'rep-des-32' => "Suicidal remarks or threatening to harm oneself",
-    'rep-title-33' => "Shocking or gory",
-    'rep-des-33' => "Shocking or graphic content",
-    'rep-title-34' => "Terrorism or act of extreme violence",
-    'rep-des-34' => "Depicting or encouraging terrorist acts or severe harm",
+    'rep-reason-3' => [
+        'text' => "Violence of physical harm",
+        'subreasons' => [
+            'rep-title-31' => "Incites violence or is a threat",
+            'rep-des-31' => "Encouraging violent acts or threatening physical harm",
+            'rep-title-32' => "Self-harm",
+            'rep-des-32' => "Suicidal remarks or threatening to harm oneself",
+            'rep-title-33' => "Shocking or gory",
+            'rep-des-33' => "Shocking or graphic content",
+            'rep-title-34' => "Terrorism or act of extreme violence",
+            'rep-des-34' => "Depicting or encouraging terrorist acts or severe harm",
+        ],
+    ],
 
-    'rep-title-41' => "Nudity or sexual content",
-    'rep-des-41' => "Nudity, sexual scenes or language, or sex trafficking",
-    'rep-title-42' => "Shocking or gory",
-    'rep-des-42' => "Shocking or graphic content",
-    'rep-title-43' => "Sexual harassment",
-    'rep-des-43' => "Unwanted romantic advances, requests for sexual favors, or unwelcome sexual remarks",
+    'rep-reason-4' => [
+        'text' => "Adult content",
+        'subreasons' => [
+            'rep-title-41' => "Nudity or sexual content",
+            'rep-des-41' => "Nudity, sexual scenes or language, or sex trafficking",
+            'rep-title-42' => "Shocking or gory",
+            'rep-des-42' => "Shocking or graphic content",
+            'rep-title-43' => "Sexual harassment",
+            'rep-des-43' => "Unwanted romantic advances, requests for sexual favors, or unwelcome sexual remarks",
+        ],
+    ],
 
-    'rep-title-5' => "Content that uncludes sexual, predatory, or abusive communications towards minors.",
+    'rep-reason-5' => [
+        'text' => "Child abuse",
+        'subreasons' => [
+            'rep-title-5' => "Content that uncludes sexual, predatory, or abusive communications towards minors.",
+        ],
+    ],
 
-    'rep-title-61' => "Copyright infringement",
-    'rep-des-61' => "Using your copyrighted work without your permission",
-    'rep-title-62' => "Trademark infringement",
-    'rep-des-62' => "Using your trademark without your permission",
-    'rep-title-63' => "Defamation",
-    'rep-des-63' => "Sharing falsehoods about you that could harm your reputation",
+    'rep-reason-6' => [
+        'text' => "Intellectual property infringement or defamation",
+        'subreasons' => [
+            'rep-title-61' => "Copyright infringement",
+            'rep-des-61' => "Using your copyrighted work without your permission",
+            'rep-title-62' => "Trademark infringement",
+            'rep-des-62' => "Using your trademark without your permission",
+            'rep-title-63' => "Defamation",
+            'rep-des-63' => "Sharing falsehoods about you that could harm your reputation",
+        ],
+    ],
 
-    'description-report' => "Description",
-    'add' => "Add",
 ];
