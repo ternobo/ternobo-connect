ce86307e329703b7261fa7f0ac63027fb4e11e24
SepehrHN <sepehrhadaeghniapv@gmail.com>
2021-06-14 01:06:28 +0430

    Currencies Name Added

--- resources/lang/en/currencies.php
+++ resources/lang/en/currencies.php
@@ -5,7 +5,7 @@ return [
     'CAD' => "CA$", //Canadian Dollar
     'CNY' => "¥", //Chinese Yuan
     'CZK' => "Kč", //Czech Koruna
-    'DKK' => "kr", //Danish Koruna
+    'DKK' => "kr", //Danish krone
     'EUR' => "€", //Euro
     'HKD' => "HK$", //Hong Kong Dollar
     'HUF' => "ft", //Hungarian Forint
@@ -16,4 +16,21 @@ return [
     'TWD' => "NT$", //New Taiwan Dollar
     'NZD' => "NZ$", //New Zealand Dollar
     'NOK' => "kr", //Norwegian Krone
+    
+    'AUD-n' => "Australian Dollar",
+    'BRL-n' => "Brazilian Real",
+    'CAD-n' => "Canadian Dollar",
+    'CNY-n' => "Chinese Yuan",
+    'CZK-n' => "Czech Koruna",
+    'DKK-n' => "Danish Koruna",
+    'EUR-n' => "Euro",
+    'HKD-n' => "Hong Kong Dollar",
+    'HUF-n' => "Hungarian Forint",
+    'JPY-n' => "Japanese Yen",
+    'IRR-n' => "Iranian Toman",
+    'MYR-n' => "Malaysian Ringgit",
+    'MXN-n' => "Mexican Peso",
+    'TWD-n' => "New Taiwan Dollar",
+    'NZD-n' => "New Zealand Dollar",
+    'NOK-n' => "Norwegian Krone",
 ];
--- resources/lang/fa/currencies.php
+++ resources/lang/fa/currencies.php
@@ -5,7 +5,7 @@ return [
     'CAD' => "CA$", //Canadian Dollar
     'CNY' => "¥", //Chinese Yuan
     'CZK' => "Kč", //Czech Koruna
-    'DKK' => "kr", //Danish Koruna
+    'DKK' => "kr", //Danish krone
     'EUR' => "€", //Euro
     'HKD' => "HK$", //Hong Kong Dollar
     'HUF' => "ft", //Hungarian Forint
@@ -16,4 +16,21 @@ return [
     'TWD' => "NT$", //New Taiwan Dollar
     'NZD' => "NZ$", //New Zealand Dollar
     'NOK' => "kr", //Norwegian Krone
+    
+    'AUD-n' => "دلار استرالیا",
+    'BRL-n' => "رئال برزیل",
+    'CAD-n' => "دلار کانادا",
+    'CNY-n' => "یوآن چین",
+    'CZK-n' => "کرونا چک",
+    'DKK-n' => "کرون دانمارک",
+    'EUR-n' => "یورو",
+    'HKD-n' => "دلار هنگ کنگ",
+    'HUF-n' => "فورینت مجارستان",
+    'JPY-n' => "ین ژاپن",
+    'IRR-n' => "تومان ایران",
+    'MYR-n' => "رینگیت مالزی",
+    'MXN-n' => "پزو مکزیک",
+    'TWD-n' => "دلار جدید تایوان",
+    'NZD-n' => "دلار نیوزیلند",
+    'NOK-n' => "کرون نروژ",
 ];
