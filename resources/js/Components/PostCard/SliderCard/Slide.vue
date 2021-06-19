c7f7ab0ce503e77a52a6b83f0cce16f3665416ca
Soroosh Tanzadeh <soroosh@ternobo.com>
2021-06-19 19:08:27 +0430

    bugs fixed

--- resources/js/Components/PostCard/SliderCard/Slide.vue
+++ resources/js/Components/PostCard/SliderCard/Slide.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="post-slide">
-		<Content v-for="content in slide" :content="content" :key="`content_item_${content.id}`"></Content>
+		<Content v-for="content in slide.content" :showFullText.sync="showFullText" :content="content" :key="`content_item_${content.id}`"></Content>
 	</div>
 </template>
 
@@ -8,10 +8,30 @@
 import Content from "../ContentElement";
 
 export default {
+	data() {
+		return {
+			showFullText: false,
+		};
+	},
+	watch: {
+		slide() {
+			this.showFullText = this.fullTextProp;
+		},
+		fullTextProp() {
+			this.showFullText = this.fullTextProp;
+		},
+		showFullText() {
+			this.$emit("update:fullTextProp", this.showFullText);
+		},
+	},
+	created() {
+		this.showFullText = this.fullTextProp;
+	},
 	components: {
 		Content,
 	},
-	props: ["slide"],
+
+	props: ["slide", "fullTextProp"],
 };
 </script>
 
