fb8836d9a1447f9487c42be32d9cf23b9aab1a5e
Soroosh Tanzadeh <soroosh@ternobo.com>
2021-06-19 12:19:12 +0430

    invite strings fixed

--- resources/lang/en/invite-links.php
+++ resources/lang/en/invite-links.php
@@ -7,7 +7,7 @@ return [
     'remaining-links' => ":number remaining",
     'what-is-inv' => "What are Invitation Links?",
     'answer-1' => "Invitation links are keys for entering Ternobo which you only have <b>2</b>.",
-    'answer-2' => "You can share your invitation links whith your friends and invite them to Ternobo.",
+    'answer-2' => "You can share your invitation links with your friends and invite them to Ternobo.",
     'attention' => "Attention",
     'answer-3' => "You can only invite 1 user per invitation link. It is not possible to create a new link.",
     'answer-4' => "You <b>must not</b> sell your invitation links. It's not acceptable to sell or auction your invitation links, both seller and buyer accounts will be banned.",
