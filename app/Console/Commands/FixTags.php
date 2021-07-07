dde2c64bfe4966a5e0fa49aeb48a51af9fdd4c5a
Soroosh Tanzadeh <soroosh@ternobo.com>
2021-07-07 19:40:41 +0430

    fix tags command updated

--- app/Console/Commands/FixTags.php
+++ app/Console/Commands/FixTags.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Post;
 use App\Models\Tag;
 use Illuminate\Console\Command;
 
@@ -42,6 +43,17 @@ class FixTags extends Command
         foreach ($tags as $tag) {
             $tag->name = str_replace(" ", "_", $tag->name);
             $tag->save();
+        }
+        $posts = Post::all();
+        foreach ($posts as $post) {
+            $tags = $post->tags;
+
+            for ($i = 0; $i < count($tags); $i++) {
+                $tags[$i] = str_replace(" ", "_", $tags[$i]);
+            }
+
+            $post->tags = $tags;
+            $tags->save();
         }
         return 0;
     }
