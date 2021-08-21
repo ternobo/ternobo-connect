456c7f1f453d2a357282f9e1050de2f28ffef1af
Soroosh Tanzadeh <soroosh@ternobo.com>
2021-08-21 10:46:56 +0430

    FixOldMediaUrls migration added

--- database/migrations/2021_08_21_060725_fix_old_media_urls.php
+++ database/migrations/2021_08_21_060725_fix_old_media_urls.php
@@ -17,7 +17,6 @@ class FixOldMediaUrls extends Migration
     {
         $blocks = DB::select("select id, content from slide_blocks where type='image' or type = 'video'");
         foreach ($blocks as $block) {
-            dd($block->id);
             if (Str::startsWith($block->content, "https://" . env("APP_URL") . "/")) {
                 $newURL = Str::replace("https://" . env("APP_URL") . "/", "", $block['content']);
                 DB::update("update slide_blocks set content = $newURL where id = " . $block->id);
