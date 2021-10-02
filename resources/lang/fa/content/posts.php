d5cfc353d3b3e3698b472e2c452600b032ae3fe6
Soroosh Tanzadeh <soroosh@ternobo.com>
2021-10-02 12:45:59 +0330

    tooltip - done

--- resources/lang/fa/content/posts.php
+++ resources/lang/fa/content/posts.php
@@ -24,8 +24,7 @@ return [
     'save' => "ذخیره",
     "tip-this-post" => "حمایت از این محتوا",
     "image-detail" => "جزئیات تصویر",
-    'spoiler-alert' => "اخطار لو رفتن",
+    'spoiler-alert' => "خطر لو رفتن",
     'rotate' => "چرخاندن",
     'alt-text' => "متن جایگزین",
-
 ];
