93396f781630d5d969a62411bd2a6a4bb13f1c4b
Soroosh Tanzadeh <soroosh@ternobo.com>
2021-11-12 16:31:17 +0330

    google tag manager added

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -36,7 +36,24 @@
                 country = response.country;
             });
     </script>
-    {{-- <script id="translationScript" async defer src="/translations.js?version={{ time() }}"></script> --}}
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-KNCCJN3');
+    </script>
+    <!-- End Google Tag Manager -->
 </head>
 
 <body dir="{{ app()->getLocale() == 'en' ? 'ltr' : 'rtl' }}" class="font-sans antialiased" data-wire='{{ $tuuid }}'>
@@ -49,6 +66,10 @@
     @else
         <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     @endif
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KNCCJN3" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 </body>
 
 </html>
