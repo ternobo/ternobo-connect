45da3edf755e77eda8b9c2336c042dd8b558bca0
Soroosh Tanzadeh <soroosh@ternobo.com>
2021-11-14 21:06:50 +0330

    visitor user signup - done

--- resources/js/Pages/Register.vue
+++ resources/js/Pages/Register.vue
@@ -62,6 +62,9 @@
 						</div>
 					</div>
 				</div>
+				<div class="invite-step" v-else-if="step == 5">
+					<visitor-user-message />
+				</div>
 			</div>
 		</div>
 	</div>
@@ -78,6 +81,7 @@ import FirstImpression from "../Components/Register/FirstImpression.vue";
 import FurtherInformation from "../Components/Register/FurtherInformation.vue";
 import PasswordEnter from "../Components/Register/PasswordEnter.vue";
 import PhoneVerification from "../Components/Register/PhoneVerification.vue";
+import VisitorUserMessage from "../Components/Register/VisitorUserMessage.vue";
 export default {
 	components: {
 		OtpInput,
@@ -90,6 +94,7 @@ export default {
 		FurtherInformation,
 		PasswordEnter,
 		LawsModal,
+		VisitorUserMessage,
 	},
 	computed: {
 		disabled() {
@@ -122,8 +127,12 @@ export default {
 		setVerificationToken(token) {
 			this.verificationToken = token;
 		},
-		goFollowings() {
-			window.location = "/feed";
+		goToLastStep() {
+			if (this.visitorRegister) {
+				this.step++;
+			} else {
+				window.location = "/feed";
+			}
 		},
 	},
 	mounted() {
