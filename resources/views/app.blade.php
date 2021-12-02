2de32fbdf14064157f28dcf65f9c2474d61a8bd7
Soroosh Tanzadeh <soroosh@ternobo.com>
2021-12-02 12:44:13 +0330

    post list bug fixed

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,7 +17,7 @@
     <link rel="stylesheet" href="/ternoboicons/style.css">
 
     {!! SEO::generate() !!}
-
+<link rel="stylesheet" href="https://uicdn.toast.com/editor/latest/toastui-editor.min.css" />
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
