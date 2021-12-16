b163398d43a0f120cd7c11e5adc75370d6956134
Soroosh Tanzadeh <soroosh@ternobo.com>
2021-12-16 14:59:06 +0330

    show comment on top post page

--- resources/js/Components/Notifications/PostViewer.vue
+++ resources/js/Components/Notifications/PostViewer.vue
@@ -1,5 +1,5 @@
 <template>
-	<wire-link class="post-content clickable" style="text-align: justify" :href="$APP_URL + '/posts/' + post.id">
+	<wire-link class="post-content clickable" style="text-align: justify" :href="href == null ? $APP_URL + '/posts/' + post.id : href">
 		<Content v-for="content in post.slides[0].content" :hideMore="true" :content="content" :key="`content_item_${content.id}`"></Content>
 	</wire-link>
 </template>
@@ -9,6 +9,9 @@ import Content from "../PostCard/ContentElement";
 export default {
 	components: { Content },
 	props: {
+		href: {
+			default: null,
+		},
 		post: {
 			type: Object,
 			default: undefined,
