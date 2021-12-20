3971b659fcc95dadca92f53e898ce14cc7e8c77a
Soroosh Tanzadeh <soroosh@ternobo.com>
2021-12-20 14:19:52 +0330

    notifications bug

--- resources/js/Components/Notifications/SkillCreditNotification.vue
+++ resources/js/Components/Notifications/SkillCreditNotification.vue
@@ -8,9 +8,9 @@
 				</div>
 				<div class="notification-text">
 					<div class="title">
-						<profile-peeking position="bottom" :page="notifications[0].sender">{{ notifications[0].sender.name }}</profile-peeking>
+						<profile-peeking position="bottom" :page="notifications[0].sender" class="hover-dark">{{ notifications[0].sender.name }}</profile-peeking>
 						<span class="mx-1" v-if="notifications[1]">{{ __.get("application.and") }}</span>
-						<profile-peeking v-if="notifications[1]" position="bottom" :page="notifications[1].sender">{{ notifications[1].sender.name }}</profile-peeking>
+						<profile-peeking v-if="notifications[1]" position="bottom" :page="notifications[1].sender" class="hover-dark">{{ notifications[1].sender.name }}</profile-peeking>
 						<span v-if="notifications.length > 2">{{ __.get("application.more-people", { number: notifications.length - 2 }) }}</span>
 						<div class="ms-2 font-12 text-gray-medium-dark font-demibold">{{ __.choice("notifications.endorsed-your-skill", notifications.length, { skill: notificationGroup.notifiable.name }) }}</div>
 					</div>
