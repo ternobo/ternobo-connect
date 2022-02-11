93b0a6f4a48a9f3bbc7877969d032540511d9674
Soroosh Tanzadeh <soroosh@ternobo.com>
2022-02-12 01:08:40 +0330

    community explore page updated

--- app/Http/Resources/CommunityTagResource.php
+++ app/Http/Resources/CommunityTagResource.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Resources;
 
+use App\Models\CommunityTranslation;
 use App\Models\Following;
 use App\Ternobo;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\App;
 
 class CommunityTagResource extends BaseResource
 {
@@ -16,12 +18,16 @@ class CommunityTagResource extends BaseResource
      */
     public function toArray($request)
     {
+        $locale = App::getLocale();
+
+        $translation = CommunityTranslation::query()->where("tag", $this->name)->where("locale", $locale)->first();
+
         return [
             "community_category_id" => $this->community_category_id,
             "cover" =>  $this->cover,
             "icon" =>  $this->icon,
             "id" =>  $this->id,
-            "name" =>  $this->name,
+            "name" =>  $translation instanceof CommunityTranslation ? $translation->translation : $this->name,
             "show_in_interst_page" =>  $this->show_in_interst_page,
             "tag" =>  $this->tag->name,
             "is_followed" => Ternobo::currentPage() ? Following::tags()->where("page_id", Ternobo::currentPage()->id)->where("following", $this->tag_id)->exists() : false,
