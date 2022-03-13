40953b096c63f86e783c0c541b42d74638b4dd70
Soroosh Tanzadeh <soroosh@ternobo.com>
2022-03-13 18:07:36 +0330

    Fix heading 1's disabled style bug fix

--- resources/js/Components/NewPost/Editor/ActionsButton.vue
+++ resources/js/Components/NewPost/Editor/ActionsButton.vue
@@ -4,53 +4,53 @@
 		<transition name="fade">
 			<div class="editors-actions-list" :style="editorsActionListStyle" v-if="showList">
 				<div class="text-type-list">
-					<div class="editor-list-item" @click="emitAcion('title')">
-						<i class="material-icons" :class="{ disabled: !activeOptions.includes('title') }">title</i>
+					<div class="editor-list-item" :class="{ disabled: !activeOptions.includes('title') }" @click="emitAcion('title')">
+						<i class="material-icons">title</i>
 						<strong>{{ __.get("editor.heading1") }}</strong>
 					</div>
-					<div class="editor-list-item" @click="emitAcion('heading2', { type: 2 })">
-						<i class="material-icons" :class="{ disabled: !activeOptions.includes('heading2') }">title</i>
+					<div class="editor-list-item" :class="{ disabled: !activeOptions.includes('heading2') }" @click="emitAcion('heading2', { type: 2 })">
+						<i class="material-icons">title</i>
 						<strong>{{ __.get("editor.heading2") }}</strong>
 					</div>
-					<div class="editor-list-item" @click="emitAcion('heading3', { type: 3 })">
-						<i class="material-icons" :class="{ disabled: !activeOptions.includes('heading3') }">title</i>
+					<div class="editor-list-item" :class="{ disabled: !activeOptions.includes('heading3') }" @click="emitAcion('heading3', { type: 3 })">
+						<i class="material-icons">title</i>
 						<strong>{{ __.get("editor.heading3") }}</strong>
 					</div>
-					<div class="editor-list-item" @click="emitAcion('text')">
-						<i class="material-icons" :class="{ disabled: !activeOptions.includes('text') }">text_fields</i>
+					<div class="editor-list-item" :class="{ disabled: !activeOptions.includes('text') }" @click="emitAcion('text')">
+						<i class="material-icons">text_fields</i>
 						<strong>{{ __.get("content/posts.text") }}</strong>
 					</div>
-					<div class="editor-list-item" @click="emitAcion('bulletedList')">
-						<i class="material-icons-outlined" :class="{ disabled: !activeOptions.includes('text') }">format_list_bulleted</i>
+					<div class="editor-list-item" :class="{ disabled: !activeOptions.includes('text') }" @click="emitAcion('bulletedList')">
+						<i class="material-icons-outlined">format_list_bulleted</i>
 						<strong>{{ __.get("editor.bulleted-list") }}</strong>
 					</div>
-					<div class="editor-list-item" @click="emitAcion('orderedList')">
-						<i class="material-icons-outlined" :class="{ disabled: !activeOptions.includes('text') }">format_list_numbered</i>
+					<div class="editor-list-item" :class="{ disabled: !activeOptions.includes('text') }" @click="emitAcion('orderedList')">
+						<i class="material-icons-outlined">format_list_numbered</i>
 						<strong>{{ __.get("editor.ordered-list") }}</strong>
 					</div>
-					<div class="editor-list-item" @click="emitAcion('quote')">
-						<i class="material-icons-outlined" :class="{ disabled: !activeOptions.includes('text') }">format_quote</i>
+					<div class="editor-list-item" :class="{ disabled: !activeOptions.includes('text') }" @click="emitAcion('quote')">
+						<i class="material-icons-outlined">format_quote</i>
 						<strong>{{ __.get("editor.quote") }}</strong>
 					</div>
 				</div>
 
 				<div class="text-type-list">
-					<div class="editor-list-item" @click="emitMediaAction('image')">
-						<i class="material-icons-outlined" :class="{ disabled: hasMedia }">image</i>
+					<div class="editor-list-item" :class="{ disabled: hasMedia }" @click="emitMediaAction('image')">
+						<i class="material-icons-outlined">image</i>
 						<strong>{{ __.get("editor.image") }}</strong>
 					</div>
-					<div class="editor-list-item" @click="emitMediaAction('video')">
-						<i class="material-icons-outlined" :class="{ disabled: hasMedia }">play_circle_outline</i>
+					<div class="editor-list-item" :class="{ disabled: hasMedia }" @click="emitMediaAction('video')">
+						<i class="material-icons-outlined">play_circle_outline</i>
 						<strong>{{ __.get("editor.video") }}</strong>
 					</div>
 				</div>
 				<div class="text-type-list">
-					<div class="editor-list-item" @click="emitAcion('code')">
-						<i class="material-icons-outlined" :class="{ disabled: !activeOptions.includes('code') }">code</i>
+					<div class="editor-list-item" :class="{ disabled: !activeOptions.includes('code') }" @click="emitAcion('code')">
+						<i class="material-icons-outlined">code</i>
 						<strong>{{ __.get("editor.code") }}</strong>
 					</div>
-					<div class="editor-list-item" @click="emitAcion('poll')">
-						<i class="material-icons-outlined" :class="{ disabled: !activeOptions.includes('poll') }">poll</i>
+					<div class="editor-list-item" :class="{ disabled: !activeOptions.includes('poll') }" @click="emitAcion('poll')">
+						<i class="material-icons-outlined">poll</i>
 						<strong>{{ __.get("editor.poll") }}</strong>
 					</div>
 				</div>
